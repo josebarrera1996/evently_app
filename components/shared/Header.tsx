40e081c549b65d11461fb05deba8e6d43bc4e469
José Barrera <josebarrerag1996@gmail.com>
2024-05-03 11:23:15 -0300

    Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
+
   return (
-    <div>Header</div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src={"/assets/images/logo.svg"}
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          {/* Clerk */}
+        </div>
+      </div>
+    </header>
   );
-}
+};
 
 export default Header;
