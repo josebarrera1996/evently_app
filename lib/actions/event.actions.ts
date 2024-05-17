190332812615ba465008db27c98507aa2ece47e5
José Barrera <josebarrerag1996@gmail.com>
2024-05-17 11:09:33 -0300

    Event Details Page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -7,7 +7,16 @@ import {
 } from '@/types';
 import User from '@/lib/database/models/user.model';
 import Event from '@/lib/database/models/event.model';
+import Category from '@/lib/database/models/category.model';
 import { handleError } from "../utils";
+
+// Lógica para poder popular el Event
+const populateEvent = (query: any) => {
+    // Además de los '_id' de las colecciones relacionadas, traer más campos de los mismos
+    return query
+        .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+        .populate({ path: 'category', model: Category, select: '_id name' })
+}
 
 /* Definiendo las acciones de servidor para interactuar con la B.D */
 
@@ -29,6 +38,24 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
 
         // Retornar el nuevo evento creado (JSON)
         return JSON.parse(JSON.stringify(newEvent));
+    } catch (error) {
+        // Invocar manejador de errores
+        handleError(error);
+    }
+}
+
+// Lógica para traer los datos de un Event (por su ID)
+export async function getEventById(eventId: string) {
+    try {
+        // Conexión a la B.D
+        await connectToDatabase();
+
+        // Intentar obtener el Event y popularlo 
+        const event = await populateEvent(Event.findById(eventId));
+        if (!event) throw new Error('Event not found');
+
+        // Retornar el Event en JSON
+        return JSON.parse(JSON.stringify(event));
     } catch (error) {
         // Invocar manejador de errores
         handleError(error);
