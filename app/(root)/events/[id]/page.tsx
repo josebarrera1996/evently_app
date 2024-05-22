6791f1feefe55c9a447152aeb55322b9e2bc1916
José Barrera <josebarrerag1996@gmail.com>
2024-05-22 10:36:30 -0300

    Profile Page (Events Organized)

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -3,6 +3,7 @@ import { SearchParamProps } from "@/types";
 import { getEventById, getRelatedEventsByCategory } from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import Collection from "@/components/shared/Collection";
+import CheckoutButton from "@/components/shared/CheckoutButton";
 
 // Componente que renderizará la info de un Event
 const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
@@ -52,8 +53,8 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
                 </p>
               </div>
             </div>
-
-            {/* <CheckoutButton event={event} /> */}
+            {/* Botón que se renderizará cuando se pueda comprar el ticket del Event */}
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
---
+++ components/shared/CheckoutButton.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+import { IEvent } from '@/lib/database/models/event.model';
+
+// Componente para poder empezar el trámite del pago del ticket
+const CheckoutButton = ({ event }: { event: IEvent }) => {
+  return (
+    <div>CheckoutButton</div>
+  )
+}
+
+export default CheckoutButton
